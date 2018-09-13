484193c4a22e0e58e036d08310ba49a5327453f9
gmanipon <gmanipon@jpl.nasa.gov>
2018-09-13 23:08:22 +0000

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='figaro',
-    version='2.0',
+    version='2.0.0',
     long_description='HySDS web ui interface',
     packages=find_packages(),
     include_package_data=True,
